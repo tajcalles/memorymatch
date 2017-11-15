35ed09c91fc8dfde888124732b8e0bde1961ab8e
Taj Calles <tajcalles@gmail.com>
2017-11-15 14:27:47 -0700

    start main js file

---
+++ main.js
@@ -0,0 +1,11 @@
+// Build a 4 x 4 or any size grid you want
+// Be able to click on each grid item and reveal the value
+// Be able to click on another item to reveal another item
+// If both items match, alert the user and keep showing those cards
+// If both items don't match, hide the values again and alert the user
+// Alert the user that they won once that matched all the items on the board
+
+
+$(document).ready( function() {
+
+});
