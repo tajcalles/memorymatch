6c78cfd9874e4832a9704a8e2583b03f32264171
Taj Calles <tajcalles@gmail.com>
2017-11-15 15:44:50 -0700

    updated images

--- main.js
+++ main.js
@@ -4,8 +4,20 @@
 // If both items match, alert the user and keep showing those cards
 // If both items don't match, hide the values again and alert the user
 // Alert the user that they won once that matched all the items on the board
+$(document).ready(function(){
+   $('.collapsible').collapsible();
+ });
 
-
-$(document).ready( function() {
-
-});
+$('.collapsible').collapsible({
+    accordion: false, // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+    onOpen: function(el) { alert('Open'); }, // Callback for Collapsible open
+    onClose: function(el) { alert('Closed'); } // Callback for Collapsible close
+  });
+// Open
+// $('.collapsible').collapsible('open', 0);
+//
+// // Close
+// $('.collapsible').collapsible('close', 0);
+//
+// // Destroy
+// $('.collapsible').collapsible('destroy');
